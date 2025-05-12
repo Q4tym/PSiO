ee3283a8a4d2357c9f9c10b1a744a39b4628db45
Hubert <hbergolec@gmail.com>
2025-05-12 12:40:09 +0200

    Zmiany by kod się kompilował

---
+++ PSiO/MyForm.cpp
@@ -0,0 +1,15 @@
+﻿#include "MyForm.h"
+
+[System::STAThread]
+int Main()
+{
+	// Inicjalizacja aplikacji
+	System::Windows::Forms::Application::EnableVisualStyles();
+	System::Windows::Forms::Application::SetCompatibleTextRenderingDefault(false);
+
+	// Tworzenie i uruchamianie g³ównego formularza
+	PSiO::MyForm^ form = gcnew PSiO::MyForm();
+	System::Windows::Forms::Application::Run(form);
+
+	return 0;
+}
