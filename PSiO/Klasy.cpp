7f65e6111285b6d21a332782353ede7e396ae508
Hubert <hbergolec@gmail.com>
2025-05-12 12:54:04 +0200

    DOdałem pliki Klasy.cpp, oraz Klasy.h do wszsytkich klas i ich metod

---
+++ PSiO/Klasy.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+
+
+class Sortwonia
+{
+	bool przyjmijPaczke();
+	{
+
+	}
+	void sortujPaczki();
+};
+
---
+++ PSiO/Klasy.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+
+
+class Sortwonia
+{
+private:
+	std::string idSortowni;
+	std::string adres;
+public:
+	bool przyjmijPaczke();
+	void sortujPaczki();
+};
+
