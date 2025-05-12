9cd35db645116d4179a521dd162e5394020325a2
Hubert <hbergolec@gmail.com>
2025-05-12 15:43:46 +0200

    Rozpisane klasy + początki metod

--- PSiO/Klasy.cpp
+++ PSiO/Klasy.cpp
@@ -4,14 +4,32 @@
 #include <fstream>
 #include <sstream>
 #include <algorithm>
+#include <Windows.h>
+
 
 
 class Sortwonia
 {
 	bool przyjmijPaczke();
 	{
-
+		bool przyjmijPaczke = false;
+		System::WindowsForms::Forms::MessageBox("Czy chcesz przyjac paczke?");
+		int wybor;
+		std::cin >> wybor;
+		if (wybor == 1)
+		{
+			przyjmijPaczke = true;
+			std::cout << "Paczka przyjeta." << std::endl;
+		}
+		else
+		{
+			przyjmijPaczke = false;
+			std::cout << "Paczka nie zostala przyjeta." << std::endl;
+		}
 	}
 	void sortujPaczki();
+	{
+
+	}
 };
 
