8d1ec0a3d3a3e3b077353ba697cc6c5138d2de23
Kamil Bijak <bijak.kamil@wp.pl>
2025-05-13 08:43:22 +0200

    przycisk wyjscia

--- PSiO/MyForm.h
+++ PSiO/MyForm.h
@@ -34,12 +34,15 @@ namespace PSiO {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	protected:
 
 	private:
 		/// <summary>
 		/// Wymagana zmienna projektanta.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+		System::ComponentModel::Container^ components;
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -48,13 +51,38 @@ namespace PSiO {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(533, 363);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(89, 40);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"zaloguj";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(12, 363);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(89, 40);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"wyjscie";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
 			// 
 			// MyForm
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(282, 253);
+			this->ClientSize = System::Drawing::Size(634, 415);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Margin = System::Windows::Forms::Padding(3, 4, 3, 4);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
 			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
@@ -64,5 +92,13 @@ namespace PSiO {
 #pragma endregion
 	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
 	}
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+		if (MessageBox::Show("Napewno chcesz zamkn¹æ aplikacjê", "Wyjscie podpowiedz", MessageBoxButtons::YesNo, MessageBoxIcon::Warning) == System::Windows::Forms::DialogResult::Yes)
+
+			Application::Exit();
+		}
 	};
 }
+
